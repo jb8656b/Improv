975e38888cb0a8d1acaccb9ae47d34c679c85d52
James Blair <jb8656b@student.american.edu>
2022-04-19 17:17:39 -0400

    chooseKey old code gone

--- Improv.js
+++ Improv.js
@@ -21,48 +21,10 @@
     }
     function chooseKey(){
         console.log("chooseKey called");
-        // var number = Math.floor(Math.random() * 12);
         const keys = new Array("A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab");
         var number = Math.floor(Math.random() * keys.length);
         console.log(number);
         let key = keys[number];
-        
-        // if(number == 0){
-        //     var key = "A";
-        // }
-        // if(number == 1){
-        //     var key = "Bb";
-        // }
-        // if(number == 2){
-        //     var key = "B";
-        // }
-        // if(number == 3){
-        //     var key = "C";
-        // }
-        // if(number == 4){
-        //     var key = "C#";
-        // }
-        // if(number == 5){
-        //     var key = "D";
-        // }
-        // if(number == 6){
-        //     var key = "Eb";
-        // }
-        // if(number == 7){
-        //     var key = "E";
-        // }
-        // if(number == 8){
-        //     var key = "F";
-        // }
-        // if(number == 9){
-        //     var key = "F#";
-        // }
-        // if(number == 10){
-        //     var key = "G";
-        // }
-        // if(number == 11){
-        //     var key = "Ab";
-        // }
         return key;
     }
     function chooseMode(){
