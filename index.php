89b5d00086302fe1fbb76bd7a1308cd561b0e1cc
James Blair <jb8656b@student.american.edu>
2022-06-16 16:54:48 -0400

    saved as php?

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!-- <html> -->
+<body>
+<script src="Improv.js"></script>
+<link href="Improv.css" rel="stylesheet">
+
+<head>
+    <title>Improv</title>
+  </head>
+<div id="mainContainer">
+    <h1>James' Improv Final</h1>
+    <div id="parameters">
+        <div id="keyPlace" class="parameter">
+            <!-- <p>test</p> -->
+        </div>
+        <div id="tempoPlace" class="parameter">
+
+        </div>
+        <div id="timeSignaturePlace" class="parameter">
+
+        </div>
+        <div id="chordProgressionPlace" class="parameter">
+
+        </div>
+    </div>
+    <div id="buttonContainer">
+        
+    </div>
+    <div id="section2" class="center">
+        <div class="slideContainer" class="center">
+            <input type="range" min="1" max="60" value="30" class="slider" id="myRange">
+            <div id="sliderNumber">
+
+            </div>
+        </div>
+        <div id="buttonContainer2">
+
+        </div>
+    </div>
+    <div id="section3" class="center">
+        <img id="image"/>
+    </div>
+</div>
+</body>
+<!-- </html> -->
